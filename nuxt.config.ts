4cbdec59902afdabbed697641ea0624264bd2e37
Gabriel <68819302+obsidianical@users.noreply.github.com>
2022-04-28 11:57:40 +0200

    Added more pug and sass to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: "vue-pug-loader",
+      },
+      {
+        test: /\.sass$/,
+        loader: "sass-loader",
+      },
+    ],
+  },
+});
